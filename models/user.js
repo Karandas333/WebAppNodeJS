66550d45828449b8779757bb527315d0e9650e9e
Karandas333 <vaishnavkaran139@gmail.com>
2024-05-11 17:05:44 +0530

    first commit

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/testapp1');
+mongoose.connect(process.env.DATABASE_URL);
 
 const userSchema = mongoose.Schema({
   image: String,
